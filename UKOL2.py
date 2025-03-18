a58fceaac14a965c0cdec6c32725ca6c6b7b36df
S3nkuCola <diana.shchelchkova@infis.cz>
2025-03-18 11:26:24 +0100

    Update UKOL2.py

--- UKOL2.py
+++ UKOL2.py
@@ -1,2 +1,11 @@
 #Úkol 2
-cislo1=float(input("Zadejte"))
+cislo1=float(input("Zadejte číslo 1: "))
+cislo2=float(input("Zadejte číslo 2: "))
+soucet=cislo1 + cislo2
+rozdil=cislo1 - cislo2
+soucin=cislo1 * cislo2
+podil=cislo1 / cislo2
+print(f"Součet je: {soucet:.2f}")
+print(f"Roydíl je: {rozdil:.2f}")
+print(f"Součin je: {soucin:.2f}")
+print(f"Podil je: {podil:.2f}")
